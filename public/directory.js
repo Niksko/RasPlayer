6a5ca3bcb6ab0e219eaeefa1d27148a792128438
Nikolas Skoufis <n.skoufis@gmail.com>
2016-02-02 14:35:01 +1100

    Add directory template, js and css
    This directory template and associated code is used to render and
    handle our folder picking interface.
    
    The template is very simple and based off of serveIndex, while the
    javascript adds in select buttons before each of the folders (as
    identified using their icon class)

---
+++ public/directory.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  // Grab all of the elements that are directories and add select buttons
+  $(".icon-directory").before('<button class="select">Select</button>');
+});
