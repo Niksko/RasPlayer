1a2adc1d566103bf961515a9fa86f2aa156e5e6d
Nikolas Skoufis <n.skoufis@gmail.com>
2016-02-02 13:43:02 +1100

    Add serve index dependency and serving

--- index.js
+++ index.js
@@ -4,5 +4,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var serveIndex = require('serve-index');
 
 var omx = require('omxdirector');
+
+app.use('/', serveIndex('/files', {'icons': true}));
+http.listen(3000);
