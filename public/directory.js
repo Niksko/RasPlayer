017a83946e15e1a366c32017861d1e26feecf7f2
Nikolas Skoufis <n.skoufis@gmail.com>
2016-02-02 14:50:01 +1100

    Remove individual select buttons in favor of one
    We want to select directories, so we only need one select button at the
    top of each page, which will allow us to select the directory itself.

--- public/directory.js
+++ public/directory.js
@@ -1,4 +1,2 @@
 $(document).ready(function() {
-  // Grab all of the elements that are directories and add select buttons
-  $(".icon-directory").before('<button class="select">Select</button>');
 });
