67756a7e58d708fae02e490b10f690c0358ed667
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-02-25 17:36:17 +1100

    Style filesystem browser

--- public/filesystem-browser.js
+++ public/filesystem-browser.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // If the select button is clicked, redirect with query string
-  $(".select").click(function(){
+  $("#select").click(function(){
     // Get the current folder location based on the link associated with the breadcrumbs at the top of the page
-    var folderLink = $(".breadcrumbs a:last-child").attr('href');
+    var folderLink = $("#breadcrumbs a:last-child").attr('href');
     // Strip the opening part of the url because this is just based on the web route
     var folderPath = folderLink.replace('/filesystem', '');
     var folderQueryString = encodeURIComponent(folderPath);
